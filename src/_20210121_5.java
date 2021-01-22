999ae33b4ea6a115213e7d5a2e14a38b6dd7adcc
OlalalalaO <1176328868@qq.com>
2021-01-22 14:57:56 +0800

    1

--- src/_20210121_5.java
+++ src/_20210121_5.java
@@ -1,2 +1,18 @@
 public class _20210121_5 {
+    public static void main(String[] args) {
+        String s = "z";
+        String s1 = "ZZ";
+        System.out.println(numJewelsInStones(s,s1));
+    }
+    public static int numJewelsInStones(String jewels, String stones) {
+        char[] jewelsArrary = jewels.toCharArray();
+        int sum = 0;
+        char[] stonesArray = stones.toCharArray();
+        for(int i = 0;i<jewels.length();i++){
+            for(int j = 0;j<stonesArray.length;j++){
+                if(jewelsArrary[i] == stonesArray[j]) sum++;
+            }
+        }
+        return sum;
+    }
 }
